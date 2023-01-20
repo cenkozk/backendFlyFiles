83dec5c04b1ff12a2dbce1971b63e759b0bf1787
Cenk Ozkan <55915851+cenkozk@users.noreply.github.com>
2023-01-20 13:31:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,76 +1,69 @@
-const app = require("express")();
+  const httpServer = require("http").createServer();
+  const cors = require("cors");
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+  const io = require("socket.io")(httpServer);
 
-const io = require("socket.io")(app, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
+  // Use cors middleware
+  io.use(cors({ origin: "*" }));
 
-var usersArr = [];
+  var usersArr = [];
 
-io.on("connection", (socket) => {
-  console.log("User connected", socket.id);
+  io.on("connection", (socket) => {
+    console.log("User connected", socket.id);
 
-  //Add user to array.
-  const mySocketID = socket.id;
+    //Add user to array.
+    const mySocketID = socket.id;
 
-  socket.on("join room", (payload) => {
-    usersArr.push({ socketId: mySocketID, sillyName: null, myIP: payload.myIP });
-    usersArr = usersArr.map((users) =>
-      users.socketId === mySocketID ? { socketId: mySocketID, sillyName: payload.mySillyName, isMobile: payload.isMobile, myIP: payload.myIP } : users
-    );
-    console.log(payload.hasOwnProperty("signal"));
-    // join the room with a combination of the IP address and socket id
-    socket.join(`${payload.myIP}`);
-    // send the "all users" event to the client
-    socket.emit(
-      "all users",
-      usersArr.filter((user) => user.myIP === payload.myIP)
-    );
-    console.log(
-      usersArr.filter((user) => user.myIP === payload.myIP),
-      "sent"
-    );
+    socket.on("join room", (payload) => {
+      usersArr.push({ socketId: mySocketID, sillyName: null, myIP: payload.myIP });
+      usersArr = usersArr.map((users) =>
+        users.socketId === mySocketID ? { socketId: mySocketID, sillyName: payload.mySillyName, isMobile: payload.isMobile, myIP: payload.myIP } : users
+      );
+      console.log(payload.hasOwnProperty("signal"));
+      // join the room with a combination of the IP address and socket id
+      socket.join(`${payload.myIP}`);
+      // send the "all users" event to the client
+      socket.emit(
+        "all users",
+        usersArr.filter((user) => user.myIP === payload.myIP)
+      );
+      console.log(
+        usersArr.filter((user) => user.myIP === payload.myIP),
+        "sent"
+      );
+    });
+
+    socket.on("sending signal", (payload) => {
+      var callerID = payload.callerID;
+      var signal = payload.signal;
+      var sillyName = payload.mySillyName;
+      var isMobile = payload.isMobile;
+      socket.broadcast.to(payload.myIP).emit("user joined", { callerID, signal, sillyName, isMobile, userToSignal: payload.userToSignal });
+    });
+
+    socket.on("returning signal", (payload) => {
+      var callerID = payload.callerID;
+      var signal = payload.signal;
+      var signalSender = socket.id;
+      socket.broadcast.to(callerID).emit("receiving returned signal", { callerID, signal, signalSender });
+    });
+
+    socket.on("msg", (msg) => {
+      console.log(`Message from ${socket.id}: ${msg}`);
+    });
+
+    socket.on("disconnect", (reason) => {
+      console.log(socket.id, "left because", '"' + reason + '"', io.of(socket.id).adapter.rooms);
+      var index = usersArr.indexOf(socket.id);
+      usersArr.splice(index, 1);
+      console.log(usersArr);
+
+      disconnectFromAll(socket.id);
+    });
   });
 
-  socket.on("sending signal", (payload) => {
-    var callerID = payload.callerID;
-    var signal = payload.signal;
-    var sillyName = payload.mySillyName;
-    var isMobile = payload.isMobile;
-    socket.broadcast.to(payload.myIP).emit("user joined", { callerID, signal, sillyName, isMobile, userToSignal: payload.userToSignal });
-  });
+  function disconnectFromAll(id, ip) {
+    io.to(ip).emit("remove disconnected", id);
+  }
 
-  socket.on("returning signal", (payload) => {
-    var callerID = payload.callerID;
-    var signal = payload.signal;
-    var signalSender = socket.id;
-    socket.broadcast.to(callerID).emit("receiving returned signal", { callerID, signal, signalSender });
-  });
-
-  socket.on("msg", (msg) => {
-    console.log(`Message from ${socket.id}: ${msg}`);
-  });
-
-  socket.on("disconnect", (reason) => {
-    console.log(socket.id, "left because", '"' + reason + '"', io.of(socket.id).adapter.rooms);
-    var index = usersArr.indexOf(socket.id);
-    usersArr.splice(index, 1);
-    console.log(usersArr);
-
-    disconnectFromAll(socket.id);
-  });
-});
-
-function disconnectFromAll(id, ip) {
-  io.to(ip).emit("remove disconnected", id);
-}
-
-app.listen(3161, console.log("listening *3161"));
+  httpServer.listen(3161, console.log("listening *3161"));
